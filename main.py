8c0dc3a214f62f098d9a8953e82696ec5b8d9e4b
ioglo <1151154@ycsh.cyc.edu.tw>
2026-09-12 15:57:47 +0800

    Initial BMI calculator

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from bmi import calculate_bmi
+from bmi import calculate_bmi, bmi_category
 
 weight = float(input("Enter your weight in kilograms:"))
 height = float(input("Enter your height in meters:"))
@@ -6,3 +6,5 @@ height = float(input("Enter your height in meters:"))
 bmi = calculate_bmi(weight, height)
 
 print(f"BMI = {bmi:.2f}")
+category = bmi_category(bmi)
+print(f"Category: {category}")
